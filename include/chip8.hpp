0f84297d5e87b3f60e46777c3f03181b98bcb3dd
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2021-02-02 14:54:26 +0200

    Initialization of chipset

--- include/chip8.hpp
+++ include/chip8.hpp
@@ -6,6 +6,12 @@
 class Chip8
 {
 public:
+    static const int MEMORY_SIZE = 4096;    // size of the memory area
+    static const char REGISTERS_SIZE = 16;  // number of registers
+    static const char STACK_SIZE = 16;      // number of supported stack levels
+    static const char KEYS_SIZE = 16;       // number of keys
+    static const int PIXELS_SIZE = 64 * 32; // number of pixels
+
     bool drawFlag; // flag tells if the display needs to be updated or not
 
     void initialize();                            // initialize the chipset
@@ -14,17 +20,17 @@ public:
     void setKeys();                               // store key press state (press and release)
 
 private:
-    unsigned char memory[4096];  // memory of the chipset
-    unsigned short opcode;       // current operation code
-    unsigned char registers[16]; // chipset registers
-    unsigned short index;        // index register
-    unsigned short pc;           // program counter
-    unsigned char gfx[64 * 32];  // pixel states (on/off)
-    unsigned char delayTimer;    // 60 Hz delay timer
-    unsigned char soundTimer;    // 60 Hz timer for sound
-    unsigned short stack[16];    // 16-level stack
-    unsigned short sp;           // stack pointer
-    unsigned char key[16];       // states of the keypad keys
+    unsigned char memory[MEMORY_SIZE];       // memory of the chipset
+    unsigned short opcode;                   // current operation code
+    unsigned char registers[REGISTERS_SIZE]; // chipset registers
+    unsigned short index;                    // index register
+    unsigned short pc;                       // program counter
+    unsigned char gfx[PIXELS_SIZE];          // pixel states (on/off)
+    unsigned char delayTimer;                // 60 Hz delay timer
+    unsigned char soundTimer;                // 60 Hz timer for sound
+    unsigned short stack[STACK_SIZE];        // 16-level stack
+    unsigned short sp;                       // stack pointer
+    unsigned char keys[KEYS_SIZE];           // states of the keypad keys
 };
 
 #endif
--- src/chip8.cpp
+++ src/chip8.cpp
@@ -3,20 +3,57 @@
 
 void Chip8::initialize()
 {
+    // initialize the memory
+    for (int i = 0; i < MEMORY_SIZE; i++)
+    {
+        memory[i] = 0;
+    }
 
+    // initialize registers
+    for (int i = 0; i < REGISTERS_SIZE; i++)
+    {
+        registers[i] = 0;
+    }
+
+    // initialize pixels
+    for (int i = 0; i < PIXELS_SIZE; i++)
+    {
+        gfx[i] = 0;
+    }
+
+    // initialize stack
+    for (int i = 0; i < STACK_SIZE; i++)
+    {
+        stack[i] = 0;
+    }
+
+    // initialize keys
+    for (int i = 0; i < KEYS_SIZE; i++)
+    {
+        keys[i] = 0;
+    }
+
+    // initialize other values
+    opcode = 0;
+    index = 0;
+    delayTimer = 0;
+    soundTimer = 0;
+    pc = 0;
+    sp = 0;
 }
 
 void Chip8::loadProgram(const std::string filename)
 {
-
 }
 
 void Chip8::emulateCycle()
 {
-
+    // Fetch Opcode
+    // Decode Opcode
+    // Execute Opcode
+    // Update timers
 }
 
 void Chip8::setKeys()
 {
-
 }
